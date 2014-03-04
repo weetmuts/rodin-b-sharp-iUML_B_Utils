0fec9920eabf5ae167961ab4d1f07eb134871818
andyed_2003@yahoo.co.uk <andyed_2003@yahoo.co.uk>
2014-03-04 16:30:12 +0000

    tele-commit auto push

--- work/store/Work/Exploratory/TestSets/TheoryPath.tcl
+++
@@ -1,6 +0,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<org.eventb.theory.core.scTheoryLanguageRoot org.eventb.core.accurate="true" org.eventb.core.configuration="org.eventb.theory.core.tul" org.eventb.core.source="/TestSets/TheoryPath.tul">
-<org.eventb.theory.core.scAvailableTheoryProject name="THPRJ_0" org.eventb.core.source="/TestSets/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'" org.eventb.theory.core.availableTheoryProject="/TestSets">
-<org.eventb.theory.core.scAvailableTheory name="THYPH_0" org.eventb.core.source="/TestSets/TheoryPath.tul|org.eventb.theory.core.theoryLanguageRoot#TheoryPath|org.eventb.theory.core.availableTheoryProject#'|org.eventb.theory.core.availableTheory#'" org.eventb.theory.core.availableTheory="/TestSets/SetImpl.dtf|org.eventb.theory.core.deployedTheoryRoot#SetImpl"/>
-</org.eventb.theory.core.scAvailableTheoryProject>
-</org.eventb.theory.core.scTheoryLanguageRoot>
