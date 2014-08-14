09ce9af8ce3bfab7df4c4606bc8858fc0d09814f
andy <andyed_2003@yahoo.co.uk>
2014-08-14 10:20:03 +0100

    Dev - IL1 Generator for VHDL

--- work/Codin/ac.soton.codin.codegen.ui./src/ac/soton/codin/codegen/basic/VHDL_IL1_DeclarationsGenerator.java
+++ work/Codin/ac.soton.codin.codegen.ui./src/ac/soton/codin/codegen/basic/VHDL_IL1_DeclarationsGenerator.java
@@ -5,7 +5,9 @@ import static org.eventb.codegen.tasking.utils.CodeGenTaskingUtils.ASSIGNMENT_SY
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eventb.codegen.il1.ConstantDecl;
@@ -37,7 +39,8 @@ public class VHDL_IL1_DeclarationsGenerator {
 
 	private Program program;
 
-	public void run(StateMachineTranslationData smTranslationMgr) {
+	public void run(StateMachineTranslationData smTranslationMgr)
+			throws CodinTranslatorException {
 		this.program = smTranslationMgr.program;
 		List<Declaration> tmpDeclarationList = new ArrayList<>();
 		// get the variables from the top-level and components.
@@ -48,6 +51,9 @@ public class VHDL_IL1_DeclarationsGenerator {
 				makeVarDeclaration(smTranslationMgr, tmpDeclarationList,
 						component, initialisation);
 			}
+			// Make constant declarations
+			makeConstDeclarations(smTranslationMgr, tmpDeclarationList,
+					component);
 		}
 		// Use a component to find the container and get the top-level
 		// variables.
@@ -63,7 +69,8 @@ public class VHDL_IL1_DeclarationsGenerator {
 						topComponent, initialisation);
 			}
 			// Make constant declarations
-			makeConstDeclarations(smTranslationMgr, tmpDeclarationList, topComponent);
+			makeConstDeclarations(smTranslationMgr, tmpDeclarationList,
+					topComponent);
 		}
 
 		// We track of signals in the smTranslatorData class. We can then map
@@ -77,10 +84,13 @@ public class VHDL_IL1_DeclarationsGenerator {
 		program.getDecls().addAll(tmpDeclarationList);
 	}
 
-	private void makeConstDeclarations(StateMachineTranslationData smTranslationMgr,
-			List<Declaration> tmpDeclarationList, Component topComponent) {
+	private void makeConstDeclarations(
+			StateMachineTranslationData smTranslationMgr,
+			List<Declaration> tmpDeclarationList, Component topComponent)
+			throws CodinTranslatorException {
 		List<ComponentConstant> constantList = topComponent.getConstants();
 		List<ComponentAxiom> axiomList = topComponent.getAxioms();
+		Map<String, ConstantDecl> cDeclMap = new HashMap<>();
 		// for each constant
 		for (ComponentConstant constant : constantList) {
 			String cName = constant.getName();
@@ -88,20 +98,66 @@ public class VHDL_IL1_DeclarationsGenerator {
 			// find an axiom that starts with the constant's
 			// name and contains the :: element of symbol.
 			// We ignore partitions (enums) for now.
-			for(ComponentAxiom axiom: axiomList){
+			for (ComponentAxiom axiom : axiomList) {
 				String predicate = axiom.getPredicate();
 				predicate = makeSingleSpaceBetweenElements(predicate);
-				if(predicate.startsWith(cName) 
-						&& predicate.contains(CodeGenTaskingUtils.ELEMENT_OF) ){
-					ConstantDecl cDecl = Il1Factory.eINSTANCE.createConstantDecl();
-					tmpDeclarationList.add(cDecl);
-					cDecl.setIdentifier(cName);
+				// if this is an initialisation axiom of the form cName = x
+				// or type axiom cName : y
+				// then we assume it is a constant Declaration with value x
+				// and type y.
+				if (predicate.startsWith(cName)
+						&& (predicate.contains(CodeGenTaskingUtils.ELEMENT_OF) || predicate
+								.contains(CodeGenTaskingUtils.EQUALS_SYMBOL))) {
+					boolean isType = predicate.startsWith(cName + " "
+							+ CodeGenTaskingUtils.ELEMENT_OF);
+
+					boolean isInitialisationEquality = predicate
+							.startsWith(cName + " "
+									+ CodeGenTaskingUtils.EQUALS_SYMBOL);
+					// if there is no declaration of cName in the map
+					ConstantDecl cDecl = null;
+					if (!cDeclMap.keySet().contains(cName)) {
+						// create a new declaration and add it to the map
+						cDecl = Il1Factory.eINSTANCE.createConstantDecl();
+						tmpDeclarationList.add(cDecl);
+						cDecl.setIdentifier(cName);
+						cDeclMap.put(cName, cDecl);
+						cDecl.setComponentName(smTranslationMgr.parentMachine.getName());
+						cDecl.setProjectName(smTranslationMgr.parentProject.getElementName());
+					}
+					// else get the existing declaration
+					else {
+						cDecl = cDeclMap.get(cName);
+					}
+					if (cDecl == null)
+						throw new CodinTranslatorException(
+								"constant name not found: " + cName);
+					if (isInitialisationEquality) {
+						// split the initialisation in the appropriate place.
+						// to find the initial value
+						String[] cInit = predicate
+								.split(CodeGenTaskingUtils.EQUALS_SYMBOL);
+						// set the initial value.
+						if (cInit != null && cInit.length > 1) {
+							cDecl.setInitialValue(cInit[1].trim());
+						}
+					} else if (isType) {
+						// find the type of the value
+						String[] cType = predicate
+								.split(CodeGenTaskingUtils.ELEMENT_OF);
+						// set the initial value.
+						if (cType != null && cType.length > 1) {
+							cDecl.setType(cType[1].trim());
+							continue;
+						}
+					}
 				}
 			}
 		}
 	}
 
-	private void makeVarDeclaration(StateMachineTranslationData smTranslationMgr,
+	private void makeVarDeclaration(
+			StateMachineTranslationData smTranslationMgr,
 			List<Declaration> tmpDeclarationList, Component component,
 			ComponentInitialisation initialisation) {
 		Declaration vDecl = Il1Factory.eINSTANCE.createVariableDecl();
@@ -123,16 +179,16 @@ public class VHDL_IL1_DeclarationsGenerator {
 
 		if (initialisationString.startsWith(variableName + " ")
 				|| hasDetAssignment || hasNonDetAssignment) {
-			String[] aInit = null;
+			String[] vInit = null;
 			// split the initialisation in the appropriate place.
 			if (hasDetAssignment) {
-				aInit = initialisationString.split(ASSIGNMENT_SYMBOL);
+				vInit = initialisationString.split(ASSIGNMENT_SYMBOL);
 			} else if (hasNonDetAssignment) {
-				aInit = initialisationString.split(ASSIGNMENT_ELEMENT_IN);
+				vInit = initialisationString.split(ASSIGNMENT_ELEMENT_IN);
 			}
 			// set the initial value.
-			if (aInit != null && aInit.length > 1) {
-				vDecl.setInitialValue(aInit[1].trim());
+			if (vInit != null && vInit.length > 1) {
+				vDecl.setInitialValue(vInit[1].trim());
 			}
 		}
 		// add this declaration to the tmp list of declarations
@@ -170,11 +226,10 @@ public class VHDL_IL1_DeclarationsGenerator {
 		boolean lastNormal = true;
 		for (char c : predIn.toCharArray()) {
 			boolean currentNormal = ((c >= 'a' && c <= 'z')
-					|| (c >= 'A' && c <= 'Z') || c == ' ' || c == '_' 
-					|| (c >= '0' && c <= '9'  ) 
-					|| CodeGenTaskingUtils.INT_SYMBOL.equals("" + c)
-					|| CodeGenTaskingUtils.BOOL_SYMBOL.equals("" + c)
-					);
+					|| (c >= 'A' && c <= 'Z') || c == ' ' || c == '_'
+					|| (c >= '0' && c <= '9')
+					|| CodeGenTaskingUtils.INT_SYMBOL.equals("" + c) || CodeGenTaskingUtils.BOOL_SYMBOL
+					.equals("" + c));
 
 			if (lastNormal && currentNormal) { // do nothing special
 			} else if (lastNormal && !currentNormal) { // add a space
