63bcd75fec4b0af19ae6007a5c3776061c85e021
andyed2003 <andyed_2003@yahoo.co.uk>
2014-12-15 15:20:55 +0000

    Removed General Info

--- work/Training/Andys/Decomposition/SimpleDecomposition/code/ada_default/mwimplpkg.ads
+++
@@ -1,9 +0,0 @@
-
-with SimpleDecomposition_Globals;
-use SimpleDecomposition_Globals;
-package mwImplPkg is 
-	protected type mwImpl is 
-		procedure send(srcAmount_p:  in Integer; srcCommand_p:  in activity);
-		procedure recv(mwAmount_p:  out Integer; mwCommand_p:  out activity);
-	end mwImpl;
-end mwImplPkg;
--- work/Training/Andys/Decomposition/SimpleDecomposition/code/ada_default/simpledecomposition_globals.ads
+++
@@ -1,5 +0,0 @@
-package SimpleDecomposition_Globals is 
-	type activity is (credit, debit);
-	for activity use 
-	(credit => 0, debit=>1);
-end SimpleDecomposition_Globals;
