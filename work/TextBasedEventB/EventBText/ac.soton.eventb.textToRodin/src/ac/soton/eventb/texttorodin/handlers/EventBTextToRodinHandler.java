16dfff97d48d4064bc4d189c4b0d86969b8c54e4
andyed2003 <andyed_2003@yahoo.co.uk>
2015-01-06 17:19:39 +0000

    More exploration of refinement, sees, extends, linkages in generation of rodin from text-based emf

--- work/TextBasedEventB/EventBText/ac.soton.eventb.textToRodin/src/ac/soton/eventb/texttorodin/handlers/EventBTextToRodinHandler.java
+++ work/TextBasedEventB/EventBText/ac.soton.eventb.textToRodin/src/ac/soton/eventb/texttorodin/handlers/EventBTextToRodinHandler.java
@@ -10,13 +10,12 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.EObjectResolvingEList;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
@@ -25,7 +24,6 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eventb.emf.core.EventBElement;
 import org.eventb.emf.core.EventBObject;
-import org.eventb.emf.core.context.Context;
 import org.eventb.emf.core.context.impl.ContextImpl;
 import org.eventb.emf.core.machine.Machine;
 import org.eventb.emf.core.machine.impl.MachineImpl;
@@ -56,6 +54,9 @@ public class EventBTextToRodinHandler extends AbstractHandler {
 		IEditorPart editorPart = HandlerUtil.getActiveEditorChecked(event);
 		Class<XtextEditor> xTextEditorClazz = XtextEditor.class;
 		Class<? extends IEditorPart> thisEditorClazz = editorPart.getClass();
+		
+		
+		
 		if (thisEditorClazz == xTextEditorClazz) {
 			IFileEditorInput input = (IFileEditorInput) editorPart
 					.getEditorInput();
@@ -86,46 +87,6 @@ public class EventBTextToRodinHandler extends AbstractHandler {
 				boolean isMachine = eClazz == machineClazz;
 				boolean isContext = eClazz == contextClazz;
 				if (isMachine || isContext) {
-					// Set the machines refinesNames to force the correct
-					// internal Rodin representation.
-					// Looks like it breaks the XText model
-					if (isMachine) {
-						Machine machine = (Machine) e;
-						List<Machine> tmpRefinesList = machine.getRefines();
-						List<String> refinesNamesList = new ArrayList<String>();
-						for (Machine refined : tmpRefinesList) {
-							String refinedMachineName = refined.getName();
-							if (!tmpRefinesList.contains(refinedMachineName)) {
-								refinesNamesList.add(refinedMachineName);
-							}
-						}
-						machine.getRefinesNames().addAll(refinesNamesList);
-
-						// List<Context> seesList = machine.getSees();
-						// List<String> tmpSeesNamesList = new
-						// ArrayList<String>();
-						// for(Context seen: seesList){
-						// String seenContextName = seen.getName();
-						// if(!tmpSeesNamesList.contains(seenContextName)){
-						// tmpSeesNamesList.add(seenContextName);
-						// }
-						// }
-						// machine.getSeesNames().addAll(tmpSeesNamesList);
-					}
-					// else if(isContext){
-					// Context context = (Context) e;
-					// EList<Context> extendsList = context.getExtends();
-					// List<String> tmpExtendsNamesList = new
-					// ArrayList<String>();
-					// for(Context extended: extendsList){
-					// String extendedName = extended.getName();
-					// if(!tmpExtendsNamesList.contains(extendedName)){
-					// tmpExtendsNamesList.add(extendedName);
-					// }
-					// context.getExtendsNames().addAll(tmpExtendsNamesList);
-					// }
-					// }
-
 					Map<IRodinElement, EventBObject> map = new HashMap<IRodinElement, EventBObject>();
 					SyncManager synchManager = new SyncManager();
 					try {
