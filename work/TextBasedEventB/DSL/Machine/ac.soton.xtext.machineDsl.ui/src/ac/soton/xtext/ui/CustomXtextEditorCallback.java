3c9a6495f44bf1eaada78ce458acd5e6189af5ad
Andrew Edmunds <ae1v08@soton.ac.uk>
2015-01-21 08:48:47 +0000

    Investigation of comment ?? Attach listener as an ITextEditorCallback, so that listener is added to the xtextEditor's TextWidget when the editor page is opened.

--- work/TextBasedEventB/DSL/Machine/ac.soton.xtext.machineDsl.ui/src/ac/soton/xtext/ui/CustomXtextEditorCallback.java
+++ work/TextBasedEventB/DSL/Machine/ac.soton.xtext.machineDsl.ui/src/ac/soton/xtext/ui/CustomXtextEditorCallback.java
@@ -2,12 +2,12 @@ package ac.soton.xtext.ui;
 
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ModifyListener;
-import org.eclipse.xtext.ui.editor.AbstractDirtyStateAwareEditorCallback;
+import org.eclipse.xtext.builder.nature.NatureAddingEditorCallback;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.rodinp.keyboard.RodinKeyboardPlugin;
 
 public class CustomXtextEditorCallback extends
-		AbstractDirtyStateAwareEditorCallback {
+		NatureAddingEditorCallback {
 
 	@Override
 	public void afterCreatePartControl(XtextEditor editor) {
