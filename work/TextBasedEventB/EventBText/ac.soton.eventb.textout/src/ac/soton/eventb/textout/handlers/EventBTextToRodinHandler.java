d7228bc44fdf3ad94f85600738adb7a79889c839
Colin Snook <cfs@ecs.soton.ac.uk>
2015-04-24 17:35:03 +0100

    reduce number of plugins: consolidate text to rodin into textout plugin

--- work/TextBasedEventB/EventBText/ac.soton.eventb.textout/src/ac/soton/eventb/textout/handlers/EventBTextToRodinHandler.java
+++ work/TextBasedEventB/EventBText/ac.soton.eventb.textout/src/ac/soton/eventb/textout/handlers/EventBTextToRodinHandler.java
@@ -1,4 +1,4 @@
-package ac.soton.eventb.texttorodin.handlers;
+package ac.soton.eventb.textout.handlers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,9 +8,13 @@ import java.util.Map;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -18,7 +22,6 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eventb.emf.core.EventBElement;
@@ -62,16 +65,10 @@ public class EventBTextToRodinHandler extends AbstractHandler {
 		Class<ContextImpl> contextClazz = ContextImpl.class;
 
 		if (thisEditorClazz == xTextEditorClazz) {
-			// Get the input from the editor UI
-			IFileEditorInput input = (IFileEditorInput) editorPart
-					.getEditorInput();
-			// This is the file associated with the editor.
-			IFile f = input.getFile();
-			URI uri = URI.createPlatformResourceURI(f.getFullPath()
+			XtextEditor xted = (XtextEditor)editorPart;
+			IResource res = xted.getResource();
+			URI uri = URI.createPlatformResourceURI(res.getFullPath()
 					.toOSString(), true);
-			// get the resource associated with the file
-			ResourceSet rs = new ResourceSetImpl();
-			Resource r = rs.getResource(uri, true);
 
 			// check we are in a rodin project
 			String directoryName = uri.segment(uri.segmentCount() - 2);
@@ -83,9 +80,17 @@ public class EventBTextToRodinHandler extends AbstractHandler {
 						"Please use an Event-B Project");
 				return null;
 			}
-
-			// get the contents of the resource
-			EObject e = r.getContents().get(0);
+			
+			TextOutUtil.addXtextNature(rodinProject.getProject());
+			
+			// get the resource associated with the file
+			ResourceSet rs = new ResourceSetImpl();
+			//ResourceSet rs = EMFRodinDB.INSTANCE.getResourceSet();	//doesn't work with the resource set used by EMFRodinDB
+			Resource r = rs.getResource(uri, true);
+			EList<EObject> c = r.getContents();
+			EObject e = c.get(0);
+			if (e==null) return null;
+			
 			List<EventBElement> toRodinList = new ArrayList<EventBElement>();
 
 			// find the event refines cross references, store in a map
@@ -107,16 +112,73 @@ public class EventBTextToRodinHandler extends AbstractHandler {
 
 	private void save(URI uri, List<EventBElement> toRodinList) {
 		for (EventBElement e : toRodinList) {
+			//resolve all xtext references
 			EcoreUtil.resolveAll(e);
+			
+			//put the model in a Rodin resource
 			URI uri2 = uri;
 			if (isMachine) {
 				uri2 = uri2.trimFileExtension().appendFileExtension("bum");
 			} else if (isContext) {
 				uri2 = uri2.trimFileExtension().appendFileExtension("buc");
 			}
+			Resource res = EMFRodinDB.INSTANCE.loadResource(uri2);
+			EList<EObject> c = res.getContents();
+			c.clear();
+			c.add(e);
+			// xtext doesn't support bidirectional associations
+			// so first we need to fix them up
+			fixEOpposites(e);
+			//save as a Rodin model
 			EMFRodinDB.INSTANCE.saveResource(uri2, e);
+			//open the Rodin editor
 			String fileName = e.getURI().lastSegment();
 			TextOutUtil.openFileForEditing(fileName , rodinProject);
 		}
 	}
+	
+	/**
+	 * This recursively traverses the model finding any reference associations that 
+	 * should be bidirectional (i.e. have an inverse). If the inverse is not set appropriately it is added
+	 * without raising notifications. 
+	 * 
+	 * @param e
+	 */
+	private void fixEOpposites(EObject e){
+		EClass clazz = e.eClass();
+		for (EReference ref : clazz.getEAllReferences()){
+			EReference oppositeRef = ref.getEOpposite();
+			if (ref.getEOpposite()!=null){
+				if (ref.isMany()){
+					for (Object r : (EList<?>)e.eGet(ref)){
+						if (r instanceof EObject){
+							secretlySet((EObject)r, oppositeRef, e);
+						}						
+					}
+				}else{
+					Object r = e.eGet(ref);
+					if (r instanceof EObject ){
+						secretlySet((EObject)r, oppositeRef, e);
+					}
+				}
+			}
+		}
+		for (EObject c : e.eContents()){
+			fixEOpposites(c);
+		}
+	}
+	
+	private void secretlySet(EObject owner, EReference referenceFeature, EObject value){
+		if (((EObject) owner).eClass().getEAllReferences().contains(referenceFeature)){
+			Object featureValue = owner.eGet(referenceFeature);
+			if ((!referenceFeature.isMany() && featureValue != value) || 
+				(featureValue instanceof EList && (!((EList<?>)featureValue).contains(value)) )){
+					boolean d = owner.eDeliver();
+					owner.eSetDeliver(false);
+					((InternalEObject)owner).eInverseAdd((InternalEObject) value, referenceFeature.getFeatureID(), owner.eClass().getClass(),  null);
+					owner.eSetDeliver(d);
+			}
+		}
+	}
+	
 }
