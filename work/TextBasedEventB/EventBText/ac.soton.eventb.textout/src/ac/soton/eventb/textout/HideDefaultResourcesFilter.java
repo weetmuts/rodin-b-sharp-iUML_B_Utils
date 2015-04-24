92d706bfe5d6a3841216bb4277c3e88f7d9b9103
Colin Snook <cfs@ecs.soton.ac.uk>
2015-04-24 17:38:05 +0100

    moved HideDefaultResources filter into top package

---
+++ work/TextBasedEventB/EventBText/ac.soton.eventb.textout/src/ac/soton/eventb/textout/HideDefaultResourcesFilter.java
@@ -0,0 +1,32 @@
+package ac.soton.eventb.textout;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+/**
+ * A Filter to hide all resource files, except for projects
+ *
+ */
+public class HideDefaultResourcesFilter  extends ViewerFilter {
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if (element instanceof IResource) {
+			if(element instanceof IFile){
+				IFile file = (IFile) element;
+				if(file.getFileExtension().equals("mch") || file.getFileExtension().equals("ctx")){
+					return true;
+				}
+			}
+			if (!(element instanceof IProject)) {
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+}
