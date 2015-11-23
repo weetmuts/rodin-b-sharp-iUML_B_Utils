5e9d3b0020575a215f46838205ef2c0d305584bd
Colin Snook <cfs@ecs.soton.ac.uk>
2015-11-23 11:44:39 +0000

    delete unused classes

--- ac.soton.iumlb.scxml.importer/src/ac/soton/iumlb/scxml/importer/rules/ScxmlDatamodelTypeRule.java
+++
@@ -1,8 +0,0 @@
-package ac.soton.iumlb.scxml.importer.rules;
-
-import ac.soton.eventb.emf.diagrams.importExport.AbstractRule;
-import ac.soton.eventb.emf.diagrams.importExport.IRule;
-
-public class ScxmlDatamodelTypeRule extends AbstractRule implements IRule {
-
-}
