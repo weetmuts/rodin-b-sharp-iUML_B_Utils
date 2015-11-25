120e45657cb15ce3c664bf8dac04e85e79ca2e7b
Colin Snook <cfs@ecs.soton.ac.uk>
2015-11-25 14:44:59 +0000

    fix copyrights

--- ac.soton.iumlb.scxml.importer/src/ac/soton/iumlb/scxml/importer/Activator.java
+++ ac.soton.iumlb.scxml.importer/src/ac/soton/iumlb/scxml/importer/Activator.java
@@ -1,20 +1,17 @@
-/**
- * Copyright (c) 2015 - University of Southampton.
- * All rights reserved. This program and the accompanying materials  are made
- * available under the terms of the Eclipse Public License v1.0 which accompanies this 
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
- * 
- *
- * $Id$
- */
+/*******************************************************************************
+ *  Copyright (c) 2015 University of Southampton.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *   
+ *  Contributors:
+ *  University of Southampton - Initial implementation
+ *******************************************************************************/
 package ac.soton.iumlb.scxml.importer;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
-
-
-
-
 
 public class Activator extends AbstractUIPlugin {
 	
