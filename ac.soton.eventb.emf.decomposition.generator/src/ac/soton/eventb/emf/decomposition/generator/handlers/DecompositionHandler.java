23ff8e48783f25cd133edacebb9b0195ad867344
Colin Snook <cfs@ecs.soton.ac.uk>
2017-03-22 13:16:57 +0000

    add post processor to decomposition to invoke flatten translator

--- ac.soton.eventb.emf.decomposition.generator/src/ac/soton/eventb/emf/decomposition/generator/handlers/DecompositionHandler.java
+++ ac.soton.eventb.emf.decomposition.generator/src/ac/soton/eventb/emf/decomposition/generator/handlers/DecompositionHandler.java
@@ -1,46 +1,60 @@
 package ac.soton.eventb.emf.decomposition.generator.handlers;
 
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eventb.emf.core.AbstractExtension;
+import org.eventb.emf.core.EventBElement;
+import org.eventb.emf.core.machine.Machine;
+import org.eventb.emf.persistence.EMFRodinDB;
 
-public class DecompositionHandler implements IHandler {
+import ac.soton.emf.translator.TranslatorFactory;
+import ac.soton.emf.translator.handler.TranslateHandler;
+import ac.soton.eventb.decomposition.AbstractRegion;
 
-	@Override
-	public void addHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
+public class DecompositionHandler extends TranslateHandler {
 
-	}
+	private static final String flattenCommandId = "ac.soton.eventb.emf.inclusion.commands.include";
+	private static final String compositionMachinePostfix = "_cmp";	
+	private static final String compositionMachineExtension = "bum";
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isHandled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void removeHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * after doing the decomposition translation, we need to call the flatten translation
+	 * 
+	 * @param sourceElement
+	 * @param commandId
+	 * @param newChild
+	 * @throws CoreException 
+	 */
+	protected void postProcessing(EObject sourceElement, String commandId, IProgressMonitor monitor) throws Exception {
+		if (sourceElement instanceof Machine){
+			EList<AbstractExtension> exts = ((Machine)sourceElement).getExtensions();
+			
+			EMFRodinDB emfRodinDB = new EMFRodinDB();
+			
+			final TranslatorFactory factory = TranslatorFactory.getFactory();
+			for (AbstractExtension ext : exts){
+				
+				if (ext instanceof AbstractRegion){
+					String compositionMachineName = ((AbstractRegion)ext).getMachineName();
+					if (compositionMachineName == null) continue;
+					compositionMachineName = compositionMachineName+compositionMachinePostfix;
+					URI fileURI = ((Machine)sourceElement).getURI();
+					fileURI = fileURI.trimFileExtension().trimSegments(1).appendSegment(compositionMachineName).appendFileExtension(compositionMachineExtension);
+					String fragment = fileURI.fragment();
+					int x = fragment.lastIndexOf("::")+2;
+					fragment = fragment.substring(0, x) + compositionMachineName;
+					fileURI = fileURI.trimFragment().appendFragment(fragment);
+					EventBElement compositionMachine = emfRodinDB.loadEventBComponent(fileURI);
+							
+					if (factory != null && compositionMachine!=null && factory.canTranslate(flattenCommandId, compositionMachine.eClass())){
+						factory.translate(compositionMachine, flattenCommandId, monitor);
+					}
+				}
+			}
+		}
 	}
 
 }
