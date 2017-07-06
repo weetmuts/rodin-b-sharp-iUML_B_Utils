a4c0a6b064da1f14e4679b780b0c0a1bec3d2a70
cfsnook <cfs@ecs.soton.ac.uk>
2017-07-06 22:08:12 +0100

    remove post process - flatten.. now done by manual command

--- ac.soton.eventb.emf.decomposition.generator/src/ac/soton/eventb/emf/decomposition/generator/handlers/DecompositionHandler.java
+++ ac.soton.eventb.emf.decomposition.generator/src/ac/soton/eventb/emf/decomposition/generator/handlers/DecompositionHandler.java
@@ -1,58 +1,20 @@
 package ac.soton.eventb.emf.decomposition.generator.handlers;
 
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eventb.emf.core.AbstractExtension;
-import org.eventb.emf.core.EventBElement;
-import org.eventb.emf.core.machine.Machine;
-import org.eventb.emf.persistence.EMFRodinDB;
 
-import ac.soton.emf.translator.TranslatorFactory;
 import ac.soton.emf.translator.handler.TranslateHandler;
-import ac.soton.eventb.decomposition.AbstractRegion;
-import ac.soton.eventb.emf.decomposition.generator.Activator;
 
 public class DecompositionHandler extends TranslateHandler {
 
 
 	/**
-	 * after doing the decomposition translation, we need to call the flatten translation
 	 * 
-	 * @param sourceElement
-	 * @param commandId
-	 * @param newChild
-	 * @throws CoreException 
+	 * Now does nothing - used to have a post processing to flatten composite machine
+	 * 
 	 */
 	protected void postProcessing(EObject sourceElement, String commandId, IProgressMonitor monitor) throws Exception {
-		if (sourceElement instanceof Machine){
-			EList<AbstractExtension> exts = ((Machine)sourceElement).getExtensions();
-			
-			EMFRodinDB emfRodinDB = new EMFRodinDB();
-			
-			final TranslatorFactory factory = TranslatorFactory.getFactory();
-			for (AbstractExtension ext : exts){
-				
-				if (ext instanceof AbstractRegion){
-					String compositionMachineName = ((AbstractRegion)ext).getMachineName();
-					if (compositionMachineName == null) continue;
-					compositionMachineName = compositionMachineName+Activator.compositionMachinePostfix;
-					URI fileURI = ((Machine)sourceElement).getURI();
-					fileURI = fileURI.trimFileExtension().trimSegments(1).appendSegment(compositionMachineName).appendFileExtension(Activator.compositionMachineExtension);
-					String fragment = fileURI.fragment();
-					int x = fragment.lastIndexOf("::")+2;
-					fragment = fragment.substring(0, x) + compositionMachineName;
-					fileURI = fileURI.trimFragment().appendFragment(fragment);
-					EventBElement compositionMachine = emfRodinDB.loadEventBComponent(fileURI);
-							
-					if (factory != null && compositionMachine!=null && factory.canTranslate(Activator.flattenCommandId, compositionMachine.eClass())){
-						factory.translate(compositionMachine, Activator.flattenCommandId, monitor);
-					}
-				}
-			}
-		}
+
 	}
 
 }
